7bc89b589bdccc5b063bf287b1af315daa5e0668
Tom Elliott <tommygunsster@gmail.com>
2020-04-24 08:22:08 -0500

    WOL needs unrestricted access at least in this test

---
+++ config/policies.js
@@ -0,0 +1,16 @@
+/**
+ * Policy Mappings
+ * (sails.config.policies)
+ *
+ * Policies are simple functions which run **before** your actions.
+ *
+ * For more information on configuring policies, check out:
+ * https://sailsjs.com/docs/concepts/policies
+ */
+
+module.exports.policies = {
+
+  // WOL requires bypass
+  'wol': true,
+  'wol/*': true
+};
