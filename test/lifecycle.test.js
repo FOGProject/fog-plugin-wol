609abc301e6e1c3410b767586e78e7daebb955ae
Tom Elliott <tommygunsster@gmail.com>
2020-04-24 11:25:12 -0500

    Remove the route tests from the lifecycle test

--- test/lifecycle.test.js
+++ test/lifecycle.test.js
@@ -18,19 +18,3 @@ before(function(done) {
 after(function(done) {
   sails.lower(done);
 });
-describe('Route /wol test::', function() {
-  it ('Should return plugin message', function(done) {
-    supertest(sails.hooks.http.app)
-    .get('/wol')
-    .expect(200)
-    .expect('{\n  "message": "FOG WOL Plugin"\n}', done);
-  });
-});
-describe('Route /wol/send/:mac test::', function() {
-  it ('Should return emit message', function(done) {
-    supertest(sails.hooks.http.app)
-    .get('/wol/send/01:02:03:04:05:06')
-    .expect(200)
-    .expect('{\n  "message": "WOL request sent to MAC: 01:02:03:04:05:06"\n}', done);
-  });
-})
